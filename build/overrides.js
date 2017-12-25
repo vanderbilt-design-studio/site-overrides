d18377073415aa0e98c19182a0f69ec4cf96c925
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-12-25 14:40:29 -0500

    Why not

---
+++ build/overrides.js
@@ -0,0 +1,136 @@
+'use strict';
+
+window.addEventListener('load', function () {
+    console.log('Applying overrides');
+    var sidebar = document.querySelector('#secnav');
+    sidebar.querySelectorAll('h4')[1].textContent = 'Schedule';
+
+    var Hours = function Hours() {
+        return React.createElement(
+            'div',
+            { className: 'textwidget' },
+            React.createElement(
+                'table',
+                null,
+                React.createElement(
+                    'tbody',
+                    null,
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Monday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '2 PM \u2013 10 PM'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Tuesday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '2 PM \u2013 10 PM'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Wednesday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '2 PM \u2013 10 PM'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Thursday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '2 PM \u2013 10 PM'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Friday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '12 PM \u2013 4 PM'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Saturday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            'Closed'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            'Sunday'
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            '4 PM \u2013 10 PM'
+                        )
+                    )
+                )
+            ),
+            React.createElement(
+                'p',
+                null,
+                'We are closed for all school holidays and breaks.'
+            ),
+            React.createElement(
+                'p',
+                null,
+                React.createElement(
+                    'a',
+                    { href: 'https://my.vanderbilt.edu/designstudio/hours/' },
+                    'Full Hours'
+                )
+            )
+        );
+    };
+
+    ReactDOM.render(React.createElement(Hours, null), sidebar.querySelectorAll('.textwidget')[1]);
+});
---
+++ overrides.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', () => {
+    console.log('Applying overrides')
+    const sidebar = document.querySelector('#secnav');
+    sidebar.querySelectorAll('h4')[1].textContent = 'Schedule';
+
+    const Hours = () => (
+        <div className="textwidget">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Monday</td>
+                        <td>2 PM – 10 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Tuesday</td>
+                        <td>2 PM – 10 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Wednesday</td>
+                        <td>2 PM – 10 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Thursday</td>
+                        <td>2 PM – 10 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Friday</td>
+                        <td>12 PM – 4 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday</td>
+                        <td>Closed</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>4 PM – 10 PM</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>We are closed for all school holidays and breaks.</p>
+            <p>
+                <a href="https://my.vanderbilt.edu/designstudio/hours/">
+                    Full Hours
+                </a>
+            </p>
+        </div>
+    );
+
+    ReactDOM.render(<Hours />, sidebar.querySelectorAll('.textwidget')[1]);
+});
