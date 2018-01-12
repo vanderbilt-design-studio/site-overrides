6e979e129fa1f20be8f68e9655b57d2bbde487e2
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-01-12 07:59:27 -0600

    Inferno/Socket.io -> Preact/WSS

--- overrides.jsx
+++ overrides.jsx
@@ -1,8 +1,7 @@
-import Inferno from 'inferno';
-import io from 'socket.io-client';
+import { h, render, Component } from 'preact';
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 import './overrides.scss';
-import DSHeader from './assets/DSHeader.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
     function clearNode(node) {
@@ -13,53 +12,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const header = document.getElementsByClassName('graphicheader')[0];
     const headerLink = header.getElementsByTagName('a')[0];
-    Inferno.render(<div className='header-image' />, clearNode(headerLink));
+    render(<div className="header-image" />, clearNode(headerLink));
 
     const sidebar = document.getElementById('secnav');
     const hoursTitle = sidebar.getElementsByTagName('h4')[1];
-    Inferno.render(<span>Schedule</span>, clearNode(hoursTitle));
+    render(<span>Schedule</span>, clearNode(hoursTitle));
 
-    const hoursWidget = sidebar.getElementsByClassName('textwidget')[1];
+    const sidebarWidget = sidebar.getElementsByClassName('textwidget')[1];
     const Hours = () => (
-        <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Monday</td>
-                        <td>2 PM – 10 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Tuesday</td>
-                        <td>2 PM – 10 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Wednesday</td>
-                        <td>2 PM – 10 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Thursday</td>
-                        <td>2 PM – 10 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Friday</td>
-                        <td>12 PM – 4 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Saturday</td>
-                        <td>Closed</td>
-                    </tr>
-                    <tr>
-                        <td>Sunday</td>
-                        <td>4 PM – 10 PM</td>
-                    </tr>
-                </tbody>
-            </table>
-            <p>We are closed during all school holidays and breaks.</p>
-        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Monday</td>
+                    <td>2 PM – 10 PM</td>
+                </tr>
+                <tr>
+                    <td>Tuesday</td>
+                    <td>2 PM – 10 PM</td>
+                </tr>
+                <tr>
+                    <td>Wednesday</td>
+                    <td>2 PM – 10 PM</td>
+                </tr>
+                <tr>
+                    <td>Thursday</td>
+                    <td>2 PM – 10 PM</td>
+                </tr>
+                <tr>
+                    <td>Friday</td>
+                    <td>12 PM – 4 PM</td>
+                </tr>
+                <tr>
+                    <td>Saturday</td>
+                    <td>Closed</td>
+                </tr>
+                <tr>
+                    <td>Sunday</td>
+                    <td>4 PM – 10 PM</td>
+                </tr>
+            </tbody>
+        </table>
     );
 
-    Inferno.render(<Hours />, clearNode(hoursWidget));
+    class SidebarSection extends Component {
+        state = {};
 
-    const socket = io('https://ds-sign.yunyul.in');
-    socket.on('status', data => console.log(data));
+        componentDidMount() {
+            const socket = new ReconnectingWebSocket('wss://ds-sign.yunyul.in');
+            socket.onmessage = ({ data }) => this.setState(JSON.parse(data));
+        }
+
+        render() {
+            return (
+                <div>
+                    <div style={{ display: 'none' }}>
+                        {JSON.stringify(this.state)}
+                    </div>
+                    <Hours />
+                    <p>We are closed during all school holidays and breaks.</p>
+                </div>
+            );
+        }
+    }
+
+    render(<SidebarSection />, clearNode(sidebarWidget));
 });
