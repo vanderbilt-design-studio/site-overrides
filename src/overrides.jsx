a0b622e15ed18326d8610afe71b23392b68144b0
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-01-25 09:22:40 -0600

    Bring back DOMContentLoaded

--- src/overrides.jsx
+++ src/overrides.jsx
@@ -4,13 +4,15 @@ import clearNode from './util/clear-node';
 import './overrides.scss';
 import SidebarSection from './SidebarSection.jsx';
 
-const header = document.getElementsByClassName('graphicheader')[0];
-const headerLink = header.getElementsByTagName('a')[0];
-render(<div className="header-image" />, clearNode(headerLink));
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementsByClassName('graphicheader')[0];
+    const headerLink = header.getElementsByTagName('a')[0];
+    render(<div className="header-image" />, clearNode(headerLink));
 
-const sidebar = document.getElementById('secnav');
-const hoursTitle = sidebar.getElementsByTagName('h4')[1];
-render(<span>Schedule</span>, clearNode(hoursTitle));
+    const sidebar = document.getElementById('secnav');
+    const hoursTitle = sidebar.getElementsByTagName('h4')[1];
+    render(<span>Schedule</span>, clearNode(hoursTitle));
 
-const sidebarWidget = sidebar.getElementsByClassName('textwidget')[1];
-render(<SidebarSection />, clearNode(sidebarWidget));
+    const sidebarWidget = sidebar.getElementsByClassName('textwidget')[1];
+    render(<SidebarSection />, clearNode(sidebarWidget));
+});
