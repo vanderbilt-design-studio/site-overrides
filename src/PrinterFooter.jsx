d5a8ac20bb1ff2b74748925c0f836e5518b0ba50
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-25 10:05:55 -0500

    Flesh out formatting into separate functions

--- src/PrinterFooter.jsx
+++ src/PrinterFooter.jsx
@@ -1,20 +1,41 @@
 import { h } from 'preact';
 import ultimaker from '../assets/ultimaker-3-1@3x.png';
 
+const formatState = (print_job) => {
+    switch (print_job.state) {
+        case 'wait_cleanup':
+            // The name of this state is unclear; it seems to simply trigger
+            // when the build plate needs to be cleaned, which happens whether the
+            // print succeeds or fails. The key to determining failure is probably
+            // checking time_elapsed < time_total but we don't have any client-side
+            // time duration library at the moment. For now, done is more clear to
+            // our customers than wait_cleanup.
+            return 'done';
+        default:
+            return print_job.state;
+    }
+};
+
+const formatStatus = (printer, print_job) => {
+    switch (printer.status) {
+        case 'printing':
+            if (print_job && print_job.state !== 'printing') {
+                return printer.status + ': ' + formatState(print_job);
+            }
+        default:
+            return printer.status;
+    }
+};
+
 export default ({ printerData }) => (
     <div class="printer-status">
-        {printerData && [...printerData].sort((a, b) => {
-            if (a.system.name > b.system.name) {
-                return 1;
-            } else if (a.system.name < b.system.name) {
-                return -1;
-            }
-            return 0;
-        }).map(({ system, printer, print_job }) => (<div class="tile">
+        {printerData && [...printerData].sort((a, b) =>
+            a.system.name.localeCompare(b.system.name)
+        ).map(({ system, printer, print_job }) => (<div class="tile">
             <img class="printer" src={ultimaker} draggable={false} />
             <div class="detail">
                 <h2 class="name">{system.name}</h2>
-                <h3 class="status">{printer.status + ((print_job && print_job.state !== 'printing' && (': ' + print_job.state)) || '')}</h3>
+                <h3 class="status">{formatStatus(printer, print_job)}</h3>
                 {print_job &&
                     <div class="time">
                         <span>{print_job.time_elapsed}</span> out of <span>{print_job.time_total}</span>
