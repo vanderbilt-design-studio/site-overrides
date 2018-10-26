09c52fd33f265adab27634ad901ad2c2426cfce3
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-26 15:35:41 -0500

    Return to replacing the text

--- src/overrides.jsx
+++ src/overrides.jsx
@@ -8,7 +8,7 @@ import PrinterFooter from './PrinterFooter.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
     const headerLink = document.getElementsByClassName('livetextheader')[0];
-    render(<div />, clearNode(headerLink));
+    render(<div className="header-image" />, clearNode(headerLink));
 
     const sidebar = document.getElementById('sidebar__right');
     const hoursTitle = clearNode(sidebar.getElementsByTagName('h4')[1]);
