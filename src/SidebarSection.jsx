20e89d63e6aaaa056c3400ccbcd3b054cdbe87cb
Sameer Puri <sameer@users.noreply.github.com>
2019-08-27 17:14:58 -0500

    Update schedule

--- src/SidebarSection.jsx
+++ src/SidebarSection.jsx
@@ -5,18 +5,16 @@ const Hours = () => (
         <tbody>
             <tr>
                 <td>Sunday</td>
-                <td>4 PM – 10 PM</td>
+                <td>2 PM – 6 PM</td>
             </tr>
             <tr>
                 <td>Monday</td>
-                <td>2 PM – 10 PM</td>
+                <td>12 PM – 10 PM</td>
             </tr>
             <tr>
                 <td>Tuesday</td>
                 <td>
-                    12 PM – 2PM
-                    <br />
-                    2:30PM - 10 PM
+                    1 PM – 10 PM
                 </td>
             </tr>
             <tr>
@@ -25,11 +23,11 @@ const Hours = () => (
             </tr>
             <tr>
                 <td>Thursday</td>
-                <td>4 PM – 10 PM</td>
+                <td>1 PM – 10 PM</td>
             </tr>
             <tr>
                 <td>Friday</td>
-                <td>12 PM – 6 PM</td>
+                <td>12 PM – 4 PM</td>
             </tr>
             <tr>
                 <td>Saturday</td>
